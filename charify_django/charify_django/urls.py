412e996e9fd74d9bc63580eaebd2529512ed7fd6
Marcin Duplaga <mnduplagan@outlook.com>
2021-11-21 14:10:20 +0100

    Changed venv files and update django

--- charify_django/charify_django/urls.py
+++ charify_django/charify_django/urls.py
@@ -1,4 +1,4 @@
-"""storefront URL Configuration
+"""charify_django URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
