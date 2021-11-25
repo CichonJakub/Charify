ef9676f42bed8be683564cde64f5a076377d08da
igabiernat <igabiernat@gmail.com>
2021-11-25 15:03:24 +0100

    event model added

--- charify_django/events/models.py
+++ charify_django/events/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 
-#  ----------------- still to work on :) ---------- in CLF 31/33
+class Event(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    address = models.TextField()
+    institution = models.CharField()
+    phone_number = models.CharField()
+    email = models.EmailField()
+    number_of_people = models.IntegerField()
+    event_date = models.DateTimeField()
+    published_date = models.DateTimeField(auto_now_add=True)
 
+#  ----------------- still to work on :) ---------- in CLF 31/33
 
 
 # class Question(models.Model):
