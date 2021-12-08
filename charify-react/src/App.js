ff66edfbd61646d7f64049bfb9ec2a87c71972ed
nbieryt <48533341+nbieryt@users.noreply.github.com>
2021-12-08 20:29:53 +0100

    commit 2

--- charify-react/src/App.js
+++ charify-react/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React from 'react';
 import axios from "axios";
+import Carousel from 'react-bootstrap/Carousel'
+import "bootstrap/dist/css/bootstrap.css";
 
 class App extends React.Component {
 
@@ -29,25 +31,29 @@ class App extends React.Component {
   }
 
 
+  
   render() {
 
     const single_event = this.state.charify_event
+
+
     return (
-      <div className="container">
+      <div>
         <div class= "topnav">
           <a class = "active" href="#wydarzenia">Wszystkie wydarzenia</a>
-          <a href = "#onas">O nas</a>
-          <a href="#kontakt">kontakt</a>
+          <a>O nas</a>
+          <a>Zgłoś wydarzenie</a>
+          <a id="login">Zaloguj się</a>
         </div>
         <div key={this.state.qid} id={this.state.qid}>
           <div id="task-container">
-            <div id="form-wrapper">
+            {/* <div id="form-wrapper"> */}
               <header className="title">
                 <h1>
                   <text>CHARIFY alpha 0.1</text>
                 </h1>
               </header>
-            </div>
+            {/* </div> */}
           </div>
           {single_event !== null ?
             <div id="task-container">
@@ -84,6 +90,41 @@ class App extends React.Component {
                   <div className="flex-button"><h5>Rekord o wpisanym ID nie istnieje</h5></div>
                 </div>
                 : <div></div>}
+                <div className='flex-wrapper' className='slider'>
+                  <h4>Najciekawsze wydarzenia</h4>
+                    <Carousel className = "carousel">
+                      <Carousel.Item interval={3500}>
+                        <img
+                          className="d-block w-100"
+                          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                          alt="Image One"/>
+                        <Carousel.Caption>
+                          <h3>Label for first slide</h3>
+                          <p>Sample Text for Image One</p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item interval={3500}>
+                        <img
+                        className="d-block w-100"
+                        src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+                        alt="Image Two"/>
+                      <Carousel.Caption>
+                        <h3>Label for second slide</h3>
+                        <p>Sample Text for Image Two</p>
+                      </Carousel.Caption>
+                      </Carousel.Item>
+                      <Carousel.Item interval={3500}>
+                        <img
+                          className="d-block w-100"
+                          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                          alt="Image One"/>
+                        <Carousel.Caption>
+                          <h3>Label for first slide</h3>
+                          <p>Sample Text for Image One</p>
+                        </Carousel.Caption>
+                      </Carousel.Item>
+                    </Carousel>
+                </div>
               <form >
                 <div className="task-wrapper flex-wrapper">
                   <input className="flex-button" type="number" value={this.state.input_value} onChange={(inp_val) => { this.setState({ input_value: parseInt(inp_val.currentTarget.value) }) }} />
@@ -91,8 +132,17 @@ class App extends React.Component {
                 </div>
               </form>
             </div>
-
           }
+        <footer>
+          <div class="container-fluid">
+              <div  id="contact">
+                <ul>
+                  <li class="glyphicon glyphicon-envelope"> charify@gmail.com    </li>
+                  <li class="glyphicon glyphicon-phone">  +48111111111    </li>
+                </ul>
+              </div>
+            </div>
+        </footer>
         </div>
       </div>
     )
