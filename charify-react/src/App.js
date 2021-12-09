6a90c9981f70ccfb9c1a0a98eaa9194effea4958
nbieryt <48533341+nbieryt@users.noreply.github.com>
2021-12-09 16:39:07 +0100

    main page

--- charify-react/src/App.js
+++ charify-react/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import axios from "axios";
 import Carousel from 'react-bootstrap/Carousel'
 import "bootstrap/dist/css/bootstrap.css";
+import background from './wave.svg';
 
 class App extends React.Component {
 
@@ -38,7 +39,7 @@ class App extends React.Component {
 
 
     return (
-      <div>
+      <div style={{ backgroundImage: `url(${background})`, backgroundSize: 'cover'}}> 
         <div class= "topnav">
           <a class = "active" href="#wydarzenia">Wszystkie wydarzenia</a>
           <a>O nas</a>
@@ -68,7 +69,7 @@ class App extends React.Component {
                 <div className="element"><h5>Data:</h5><span>{single_event.event_date}</span></div>
                 <div className="element"><h5>Tagi:</h5><span>{single_event.tags}</span></div>
                 <div id="form-wrapper">
-                  <header className="title">
+                  <header className="opis">
                     <h2>Opis</h2>
                     <text>{single_event.description}</text>
                   </header>
@@ -90,13 +91,19 @@ class App extends React.Component {
                   <div className="flex-button"><h5>Rekord o wpisanym ID nie istnieje</h5></div>
                 </div>
                 : <div></div>}
-                <div className='flex-wrapper' className='slider'>
+                <form >
+                <div className="task-wrapper flex-wrapper">
+                  <input className="flex-button"  value={this.state.input_value} onChange={(inp_val) => { this.setState({ input_value: parseInt(inp_val.currentTarget.value) }) }} />
+                  <button type="button" className="button" onClick={() => { this.setState({ qid: this.state.input_value }, this.fetchData) }}>Przejdz do rekordu o podanym ID</button>
+                </div>
+                </form>
+                <div className="slider" align="center">
                   <h4>Najciekawsze wydarzenia</h4>
                     <Carousel className = "carousel">
                       <Carousel.Item interval={3500}>
                         <img
                           className="d-block w-100"
-                          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
+                          src={"./event1.jpg"}
                           alt="Image One"/>
                         <Carousel.Caption>
                           <h3>Label for first slide</h3>
@@ -106,31 +113,15 @@ class App extends React.Component {
                       <Carousel.Item interval={3500}>
                         <img
                         className="d-block w-100"
-                        src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122716/1-300x115.png"
+                        src={"./event2.jpg"}
                         alt="Image Two"/>
                       <Carousel.Caption>
                         <h3>Label for second slide</h3>
                         <p>Sample Text for Image Two</p>
                       </Carousel.Caption>
                       </Carousel.Item>
-                      <Carousel.Item interval={3500}>
-                        <img
-                          className="d-block w-100"
-                          src="https://media.geeksforgeeks.org/wp-content/uploads/20210425122739/2-300x115.png"
-                          alt="Image One"/>
-                        <Carousel.Caption>
-                          <h3>Label for first slide</h3>
-                          <p>Sample Text for Image One</p>
-                        </Carousel.Caption>
-                      </Carousel.Item>
                     </Carousel>
                 </div>
-              <form >
-                <div className="task-wrapper flex-wrapper">
-                  <input className="flex-button" type="number" value={this.state.input_value} onChange={(inp_val) => { this.setState({ input_value: parseInt(inp_val.currentTarget.value) }) }} />
-                  <button type="button" className="flex-button" onClick={() => { this.setState({ qid: this.state.input_value }, this.fetchData) }}>Przejdz do rekordu o podanym ID</button>
-                </div>
-              </form>
             </div>
           }
         <footer>
