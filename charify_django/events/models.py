e48acefb9b78dfadd07a34acc4aa0f5ad830da4d
bgdowski <b.gdowski620@gmail.com>
2021-12-14 12:22:04 +0100

    gitingore & django models

--- charify_django/events/models.py
+++ charify_django/events/models.py
@@ -1,22 +1,23 @@
 from django.db import models
+from django.core.validators import EmailValidator, MaxValueValidator
 from django.core.exceptions import ValidationError
 
 
 class Event(models.Model):
     title = models.CharField(max_length=150,
-                            required=True)
-    description = models.TextField(required=True)
-    address = models.TextField(required=True)
-    organizer = models.CharField(max_length=150, required=True)
+                            blank=False)
+    description = models.TextField(blank=False)
+    address = models.TextField(blank=False)
+    organizer = models.CharField(max_length=150, blank=False)
     organizer_type = models.CharField(max_length=150)
     phone_number = models.CharField(max_length=9)
-    email = models.EmailField(validators=EmailValidator("Enter a valid email"))
-    number_of_people = models.IntegerField(required=True,
-                                           validators=MaxValueValidator(999, "Too much people"))
-    event_date = models.DateTimeField(required=True)
+    email = models.EmailField(validators=[EmailValidator("Enter a valid email")])
+    number_of_people = models.IntegerField(blank=False,
+                                           validators=[MaxValueValidator(999, "Too much people")])
+    event_date = models.DateTimeField(blank=False)
     published_date = models.DateTimeField(auto_now_add=True)
     tags = models.CharField(max_length=150)
-    photo = models.ImageField(blank=True)
+    photo = models.ImageField(blank=False)
 
     def _str_(self):
         return self.title
